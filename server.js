b3407455405cd636d66d4d5391d31eebec2d5f4c
Ryan Baird <rjbaird09@gmail.com>
2020-07-20 20:10:55 -0500

    update for refactor

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = process.env.PORT || 3306;
-
+// Initialize the app and create a port
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("/public"));
-
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+// Set up body parsing, static, and route middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// Import routes and give the server access to them.
-const apiRoutes = require("./controllers/notesController.js");
-const htmlRoutes = require("./controllers/htmlController.js");
-
-app.use(apiRoutes);
-app.use(htmlRoutes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at http://localhost:" + PORT);
-});
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
